96f876c5dade1efc37a8e0ad85a42bbce8824708
Adam <kurskievadam001@gmail.com>
2021-06-04 17:25:48 +0300

    'v1'

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Привет</h1>
+    <h3>Меня зовут Адам. Ниже есть крутая фотка</h3>
+    <img :src="'avatar.png'" />
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Привет еще раз!')
+    }
+  }
+</script>
